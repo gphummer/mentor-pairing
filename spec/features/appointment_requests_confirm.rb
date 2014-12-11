4ae2a277f310c82e4ffa290c63517cbfe620aa7e
Thomas Lee & Grant Hummer <pair+djvoa12.gphummer@devbootcamp.com>
2014-12-11 15:27:46 -0600

    comments

--- spec/features/appointment_requests_confirm.rb
+++ spec/features/appointment_requests_confirm.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
+
+
 feature "User requests an appointment" do
+  before do
+    time = Time.now
+
+    # User.create!()
+    # Availability.create!(start_time: time, end_time: time + 1.hour, mentor_id: 1, city: "Chicago",
+    #                        timezone: "America/Chicago")
+  end
+
   scenario "when user creates a new mentoring request" do
-    visit '/appointment_requests/new?availability_id=2'
+    availability = Availability.last.id
+    visit "/appointment_requests/new?availability_id=#{availability}"
+
     fill_in "first_name", with: "teeee"
     fill_in "last_name", with: "money"
     fill_in "twitter_handle", with: "makarnamasdf"
@@ -13,6 +25,7 @@ feature "User requests an appointment" do
     # AppointmentRequest.any_instance.stub(ar_param)
 
     click_on 'submit'
-    expect(page).to have_text("Thanks for signing up!!")
+
+    expect(page).to have_text("Thanks for signing up!!!!")
   end
 end
